3dad53fe15a484f46a5f795dae6e228da6c5981c
azmax <c0a23005fa@edu.teu.ac.jp>
2024-11-19 16:36:56 +0900

    練習8　こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,22 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        print(key_lst[pg.K_UP], key_lst[pg.K_DOWN],
+              key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
+        
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1)
+
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)    
+
         x = -(tmr%3200)
         screen.blit(bg_img,  [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
